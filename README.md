aaad0de245363defd5b840f6724fb8ee46e8ca48
David Ruiz <davidmw4@hotmail.com>
2022-05-21 21:21:19 -0500

    Heroku integration

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Laptop price calculator with IA
+# Laptop price prediction with IA
 
 ### This is a simple web app that calculates the price of a laptop based on parameters and use AWS services like SageMaker, API Gateway and Lambda to predict the price.
 
--- app.js
+++ app.js
@@ -11,17 +11,16 @@ app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-app.get('/', async (req, res) => {
+app.get('/.*/', async (req, res) => {
     res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
 app.post('/make-prediction', async (req,res) => {
     const prediction = await makePrediction(req, res);
-    console.log(prediction);
     if (prediction) return res.json({
         prediction
     });
-    res.status(404).send("Error");
+    return res.status(404).send("Error");
 });
 
 app.listen(port, () => {
